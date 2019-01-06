8eb65b755e9eb78e0371e011c2e81e8c28454761
Alessandro Gottardi <alessandrogottardi00@gmail.com>
2019-01-06 15:50:43 +0100

    Change the list endpoints logic in out-of-range page condition If an out of range page was chosen, the server now replies with the latest available page

--- lib/libList/include.php
+++ lib/libList/include.php
@@ -36,24 +36,27 @@ function get_list_data(string $type, array $data, DatabaseWrapper $db, $cur_user
         //AND ((SELECT area_id FROM users WHERE author_user_id = user_id) LIKE (SELECT area_id FROM users WHERE user_id = $cur_user_id))
     }
 
-    //Prepopulate the response array
-    $out = [
-        'count' => 0,
-        'count_total' => count($data),
-        'list' => [],
-        'page' => $req['page'],
-        'page_total' => 0
-    ];
-
+    
     //Calculate the number of max pages based on the limit (if it's a float number, round by excess)
     $max_page = count($data) / $req['limit'];
     if(is_float($max_page))
         $max_page = to_int($max_page) + 1;
 
-    $out['page_total'] = $max_page;
+    //Verify if the chosen page number is above the total number of pages: in that case, output the latest available page
+    $page = $req['page'];
+    if ($page > $max_page) $page = $max_page;
+
+    //Prepopulate the response array
+    $out = [
+        'count' => 0,
+        'count_total' => count($data),
+        'list' => [],
+        'page' => $page,
+        'page_total' => $max_page
+    ];
 
     //Populate the response array with the commit elements of the chosen page
-    for ($i=($req['page'] * $req['limit']); $i<(($req['page']+1) * $req['limit']); $i++) {
+    for ($i=($page * $req['limit']); $i<(($page+1) * $req['limit']); $i++) {
         $entry = $data[$i];
 
         if ($entry == NULL)
