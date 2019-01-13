9af808078d432ce6134e4d666359a2b42e9ac8b6
Alessandro Gottardi <alessandrogottardi00@gmail.com>
2019-01-13 18:50:35 +0100

    Filter commits/requests by area

--- lib/libList/include.php
+++ lib/libList/include.php
@@ -4,7 +4,7 @@ function to_int($i) : int {
     return (int) $i;
 }
 
-function get_list_data(string $type, array $data, DatabaseWrapper $db, $cur_user_id){
+function get_list_data(string $type, array $data, DatabaseWrapper $db, $cur_user_id, $cur_user_role) {
     $req = $data;
     $type = strtolower($type);
 
@@ -12,28 +12,44 @@ function get_list_data(string $type, array $data, DatabaseWrapper $db, $cur_user
     //The LIKE part looks like this: attribute _/NOT LIKE %valueMatches%
     switch ($type){
         case "commit":
-            $data = $db->query(
-            "SELECT users.username, users.name, commit.*
-             FROM commit, users
-             WHERE commit.author_user_id = users.user_id AND {$data['filter']['attribute']} {$data['filter']['negate']} LIKE '%{$data['filter']['valueMatches']}%'
-             ORDER BY {$data['sort']['parameter']} {$data['sort']['order']}"
-            );
+            $query = "SELECT users.username, users.name, commit.*
+            FROM commit, users
+            WHERE commit.author_user_id = users.user_id AND {$data['filter']['attribute']} {$data['filter']['negate']} LIKE '%{$data['filter']['valueMatches']}%'";
+            
+            //If the user is part of the tech area or is a programmer, select only users in the same area
+            if ($cur_user_role[0] == 2 || $cur_user_role[4]) {  // Tech Area
+                $area = $db->query("SELECT area_id FROM users WHERE user_id = {$cur_user_id}")[0]['area_id'];
+                $query = $query . " AND (SELECT area_id FROM users WHERE author_user_id = user_id) = {$area}";
+            }
+
+            //Append the last part of the query and execute it
+            $query .= " ORDER BY {$data['sort']['parameter']} {$data['sort']['order']}";
+            $data = $db->query($query);
+
             $id = "commit_id";
             $author = "author_user_id";
             break;
         case "request":
-            $data = $db->query(
-            "SELECT users.username, users.name, requests.*
-             FROM requests, users
-             WHERE requests.requester = users.user_id AND {$data['filter']['attribute']} {$data['filter']['negate']} LIKE '%{$data['filter']['valueMatches']}%'
-             ORDER BY {$data['sort']['parameter']} {$data['sort']['order']}"
-            );
+            $query = "SELECT users.username, users.name, requests.*
+            FROM requests, users
+            WHERE requests.requester = users.user_id AND {$data['filter']['attribute']} {$data['filter']['negate']} LIKE '%{$data['filter']['valueMatches']}%'
+            ORDER BY {$data['sort']['parameter']} {$data['sort']['order']}";
+
+            //If the user is part of the tech area or is a programmer, select only users in the same area
+            if ($cur_user_role[0] == 2 || $cur_user_role[4]) {  // Tech Area
+                $area = $db->query("SELECT area_id FROM users WHERE user_id = {$cur_user_id}")[0]['area_id'];
+                $query = $query . " AND (SELECT area_id FROM users WHERE requester = user_id) = {$area}";
+            }
+
+            //Append the last part of the query and execute it
+            $query .= " ORDER BY {$data['sort']['parameter']} {$data['sort']['order']}";
+            $data = $db->query($query);
+            
             $id = "request_id";
             $author = "requester";
             break;
         default:
             throw new Exception("Impossible to use the list");
-        //AND ((SELECT area_id FROM users WHERE author_user_id = user_id) LIKE (SELECT area_id FROM users WHERE user_id = $cur_user_id))
     }
 
     
