b57cdb44930684abbef0a8319a80bf926f60df5a
Alessandro Gottardi <alessandrogottardi00@gmail.com>
2019-02-03 16:21:03 +0100

    Whoops

--- modules/commit/approve.php
+++ modules/commit/approve.php
@@ -8,7 +8,7 @@ $exec = function (array $data, array $data_init) : array {
     require_once __DIR__ . "/../../lib/libCommitRequest/libApprove.php";
 
     $user = getUserData($db, $token);
-    approve($db, $data, $user, "request");
+    approve($db, $data, $user, "commit");
 
     return [
         "response_data" => [],
