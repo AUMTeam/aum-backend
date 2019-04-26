077da8f2990f84d252f04f167dd83f4a4980930a
Alessandro Gottardi <alessandrogottardi00@gmail.com>
2019-04-26 10:45:01 +0200

    Implement getBranch and getClients

---
+++ modules/data/getBranch.php
@@ -0,0 +1,25 @@
+<?php
+
+$init = function (array $data) : array { return []; };
+
+/**
+ * Get the list of branches present in the DB
+ */
+$exec = function (array $data, array $data_init) : array {
+    global $db;
+
+    $data = $db->preparedQuery("SELECT * FROM branches");   //users
+    $out = [];
+
+    foreach ($data as $entry) {
+        $out[] = [
+            'id' => $entry['branch_id'],
+            'name' => $entry['branch_name']
+        ];
+    }
+
+    return [
+        "response_data" => $out,
+        "status_code" => 200
+    ];
+};
