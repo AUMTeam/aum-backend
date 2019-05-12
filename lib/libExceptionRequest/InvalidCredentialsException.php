4b2218812b43ea55c8d0db34e584f60053fa4787
Alessandro Gottardi <alessandrogottardi00@gmail.com>
2019-05-12 13:59:36 +0200

    Return 401 even if user is not present in the DB

--- lib/libExceptionRequest/InvalidCredentialsException.php
+++ lib/libExceptionRequest/InvalidCredentialsException.php
@@ -2,7 +2,7 @@
 
 class InvalidCredentialsException extends ExceptionRequest {
 
-    public function __construct(string $message, string $error_code = "ERROR_LOGIN_INVALID_CREDENTIALS", int $status_code = 401) {
+    public function __construct(string $message = "Username or password are wrong", string $error_code = "ERROR_LOGIN_INVALID_CREDENTIALS", int $status_code = 401) {
         parent::__construct($message, $error_code, $status_code);
     }
 
