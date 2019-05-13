3a8b8ed726f6b5dd277f77ef3879aec83d531bdc
Alessandro Gottardi <alessandrogottardi00@gmail.com>
2019-05-13 15:07:08 +0200

    Update endpoints: simplify

--- lib/libCommitRequest/libUpdate.php
+++ lib/libCommitRequest/libUpdate.php
@@ -4,49 +4,40 @@
  * Check if there are new commits/requests for the current user,
  * given a timestamp
  */
-function getUpdates(array $data, string $type) : array {
-    global $db;
-    global $user;
-    $last = null;
-    $out = [];
-    
+function getUpdates(array $data, string $type) : array {    
     //Check parameters presence
     if(!isset($data['latest_update_timestamp']) || empty($data['section']))
         throw new InvalidRequestException("latest_update_timestamp cannot be blank", "ERROR_COMMIT_UPDATE_NO_TIMESTAMP");
 
     //Check $type
-    $id_name;
     switch ($type) {
         case TYPE_COMMIT:
-            $id_name = TYPE_COMMIT_ID;
-            break;
         case TYPE_REQUEST:
-            $id_name = TYPE_REQUEST_ID;
             break;
         default:
-            throw new Exception("Impossible to use the endpoint");
+            throw new InvalidRequestException("Impossible to use the endpoint");
     }
+
+    global $db;
+    global $user;
+    $last = null;
+    $out = [];
     
     //Check the latest update timestamp based on the section the user is visiting
     switch ($data['section']) {
         case ROLE_DEVELOPER:    //A developer sees only its commits
-            $creat = $db->preparedQuery("SELECT MAX(creation_timestamp) as last FROM $type WHERE author_user_id=?", [$user['user_id']])[0]['last'];
-            $last = $db->preparedQuery("SELECT MAX(approvation_timestamp) as last FROM $type WHERE author_user_id=?", [$user['user_id']])[0]['last'];
-            if ($creat > $last) $last = $creat;
+            $last = $db->preparedQuery("SELECT GREATEST(MAX(creation_timestamp), MAX(approvation_timestamp)) as last FROM $type WHERE author_user_id=?", [$user['user_id']]);
+            break;
         case ROLE_TECHAREA:     //A member of a tech area sees all the commits from its area
             $area = $db->preparedQuery("SELECT area_id FROM users WHERE user_id=?", [$user['user_id']])[0]['area_id'];
 
-            $creat = $db->preparedQuery("SELECT MAX(creation_timestamp) as last FROM $type WHERE (SELECT area_id FROM users WHERE author_user_id=user_id) = {$area}")[0]['last'];
-            $last = $db->preparedQuery("SELECT MAX(approvation_timestamp) as last FROM $type WHERE (SELECT area_id FROM users WHERE author_user_id=user_id) = {$area}")[0]['last'];
-            if ($creat > $last) $last = $creat;
+            $last = $db->preparedQuery("SELECT GREATEST(MAX(creation_timestamp), MAX(approvation_timestamp)) as last FROM $type WHERE (SELECT area_id FROM users WHERE author_user_id=user_id) = {$area}");
             break;
         case ROLE_REVOFFICE:    //A member of the revision office sees all the approved requests
-            $last = $db->preparedQuery("SELECT MAX(approvation_timestamp) as last FROM requests WHERE approval_status IN ('1')")[0]['last'];
-            $send = $db->preparedQuery("SELECT MAX(send_timestamp) as last FROM requests")[0]['last'];
-            if ($send > $last) $last = $send;
+            $last = $db->preparedQuery("SELECT GREATEST(MAX(approvation_timestamp), MAX(send_timestamp)) as last FROM requests WHERE approval_status IN ('1')");
             break;
         case ROLE_CLIENT:       //A client sees only its updates 
-            $last = $db->preparedQuery("SELECT MAX(send_timestamp) as last FROM requests, requests_clients WHERE client_user_id=?", [$user['user_id']])[0]['last'];
+            $last = $db->preparedQuery("SELECT MAX(send_timestamp) as last FROM requests, requests_clients WHERE client_user_id=?", [$user['user_id']]);
             break;
         default:
             throw new InvalidRequestException("Invalid section parameter!");
@@ -54,7 +45,7 @@ function getUpdates(array $data, string $type) : array {
     }
 
     //Convert date to string if present, otherwise keep NULL as the result
-    $last = ($last != null) ? strtotime($last) : null;
+    $last = (count($last) > 0) ? strtotime($last[0]['last']) : null;
 
     $out = [
         "latest_update_timestamp" => $last,
