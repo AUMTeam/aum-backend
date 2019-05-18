dc27f417d14beb8fdcb0afe2d80276f6c2d70783
Alessandro G <alessandrogottardi00@gmail.com>
2019-05-18 08:21:00 +0200

    Return more appropriate 501 code if endpoint is not implemented

--- lib/libExceptionRequest/NotImplementedException.php
+++ lib/libExceptionRequest/NotImplementedException.php
@@ -2,7 +2,7 @@
 
 class NotImplementedException extends ExceptionRequest {
 
-    public function __construct(string $message, string $error_code = "ERROR_GLOBAL_NOT_IMPLEMENTED", int $status_code = 404) {
+    public function __construct(string $message, string $error_code = "ERROR_GLOBAL_NOT_IMPLEMENTED", int $status_code = 501) {
         parent::__construct($message, $error_code, $status_code);
     }
 
